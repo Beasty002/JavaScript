042069b00b653cd7fc33fbc52eb3bc5c5deb8122
Beasty002 <xasbin12@gmail.com>
2024-06-12 14:35:46 +0545

    done operators and conditional statements

--- 2.operation&conditional-Statements/conditional.js
+++ 2.operation&conditional-Statements/conditional.js
@@ -42,3 +42,58 @@ else {
     console.log("you are a middle");
 
 }
+
+
+//---------------------------------------------------Ternary operators
+
+// syntax :  condition?true o/p : false o/p
+// eg a?b:c     , if a is true then execute b else execute c
+
+age = 40;
+let result = age > 20 ? "adult" : "not adult";
+console.log(result);
+
+// -------------------------------switch statement
+
+const name = 'beasty';
+switch (name) {
+    case 'nick': console.log('hey nick'); break;
+    case 'Rick': console.log('hey rick'); break;
+    case 'beasty': console.log('hey beasty'); break;
+}
+
+
+//--------------------------practice question to check if the number is multiple of 5
+// let num = prompt("enter a number");
+// console.log(num);
+// if (num % 5 === 0) {
+//     alert(num + " is multiple of 5");
+//     // in alert use '+' instead of comma to concatenate the string or any data types
+// }
+// else {
+//     alert(num + "is not multiple of 5"); 4
+// }
+
+// -------------------code to give student a grade
+let grade = prompt("enter your grade")
+if (grade > 100 || grade < 0) {
+    alert("enter a valid grade");
+}
+else if (grade >= 90 && grade <= 100) {
+    alert("Congrats you got A");
+}
+else if (grade >= 70 && grade <= 89) {
+    alert("Congrats you got B");
+}
+else if (grade >= 60 && grade <= 69) {
+    alert("Congrats you got C");
+}
+else if (grade >= 50 && grade <= 59) {
+    alert("Congrats you got D");
+}
+else {
+    alert("Try again")
+}
+
+
+
