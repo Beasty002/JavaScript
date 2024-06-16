dd53b908154177c9268483324d4a3aa1650529a8
Beasty002 <xasbin12@gmail.com>
2024-06-16 22:36:02 +0545

    done basic class and constructor

---
+++ 9.Classes&Objects/class.js
@@ -0,0 +1,25 @@
+// class is a program code template for creating objects.
+// Those objects will have some state (variables) and soem behaviour (functions) inside it
+// ---------------------------------------------------Constructor
+// it is a special keyboard that gets automatically called when new object is created
+// It is mainly used to initialize the values
+
+class ToyotaCar {
+    constructor(msg) {
+        console.log("I have been called from constructor", msg);
+    }
+    start() {
+        console.log("start");
+    }
+    stop() {
+        console.log("stop")
+    }
+    setBrand(model) {
+        this.modelName = model; // we can have both the name same modeName and model
+        console.log(`Your model is ${this.modelName}`)
+    }
+}
+let fortuner = new ToyotaCar("Helo");
+fortuner.start();
+fortuner.stop();
+fortuner.setBrand("MXY24")
