541a61590737b8440ff613fbe2aaca730226d834
Beasty002 <xasbin12@gmail.com>
2024-06-16 23:00:03 +0545

    done try catch

---
+++ 9.Classes&Objects/error.js
@@ -0,0 +1,14 @@
+let a = 5;
+let b = 2;
+let c = 0;
+console.log(a + b);
+console.log(a - b);
+console.log(a + c);
+try {
+    console.log(a / d);
+
+} catch (err) {
+    console.log(err)
+}
+// if any error would occur then the code would stop at that point of error now with try and catch woulnt stop
+console.log(a - c);
