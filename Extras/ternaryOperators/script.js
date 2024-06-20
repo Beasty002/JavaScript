672a27f360ad2b74cf65b49a632289852dded2ae
Beasty002 <xasbin12@gmail.com>
2024-06-20 19:20:42 +0545

    done ternary operator

---
+++ Extras/ternaryOperators/script.js
@@ -0,0 +1,17 @@
+const num = 5;
+let result;
+// if (num >= 50) {
+//     result = "not fifty";
+// }
+// else {
+//     result = "it could be worse"
+// }
+
+//can be written as
+
+// condition ? if value(true) : else value (false)
+
+
+result = num >= 50 ? "not fifty" : "it could be worse"
+
+console.log(result);
