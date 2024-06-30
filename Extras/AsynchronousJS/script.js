c993ca1ba647dcf16df63250cac6f7c23f9fd74c
Beasty002 <xasbin12@gmail.com>
2024-06-30 22:26:17 +0545

    created and accessed json file

--- Extras/AsynchronousJS/script.js
+++ Extras/AsynchronousJS/script.js
@@ -7,24 +7,43 @@
 // JS is a non-blocking language
 // this means that js can perform multiple tasks at the same time by using callbacks and promises
 
-console.log(1);
-console.log(2);
-setTimeout(() => {  //this is asynchronous and doesnt block the code andis executed after 2s
+// console.log(1);
+// console.log(2);
+// setTimeout(() => {  //this is asynchronous and doesnt block the code andis executed after 2s
+//     console.log("callback fired");
+// }, 2000);
+// console.log(3);
+// console.log(4);
+
+const getTodos = (callback) => {
+    const request = new XMLHttpRequest();
+    request.addEventListener("readystatechange", () => {
+        // console.log(request, request.readyState)
+        if (request.readyState === 4 && request.status === 200) {
+            // console.log(request.responseText);
+            // callback(undefined, request.responseText) //resonse text holds the data in json format
+            const data = JSON.parse(request.responseText) //converting the data from json to json format
+            callback(undefined, data); // here we got array of data
+        }
+        else if (request.readyState === 4) {
+            callback('could not fetch data', undefined);
+        }
+    })
+    request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
+    // request.open('GET', './todos.json'); //we can also use out own custom json file
+    request.send();
+
+}
+
+getTodos((err, data) => { //always error first and then data
     console.log("callback fired");
-}, 2000);
-console.log(3);
-console.log(4);
-
-const request = new XMLHttpRequest();
-request.addEventListener("readystatechange", () => {
-    // console.log(request, request.readyState)
-    if (request.readyState === 4 && request.status === 200) {
-        console.log(request.responseText);
+    if (err) {
+        console.log(err);
     }
-})
-request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
-request.send();
-
+    else {
+        console.log(data);
+    }
+});
 
 
 
